44d22b7eccaaadac6191c788b12704e461cad365
diefbell <diefenbaker.bell@gmail.com>
2022-03-19 21:12:54 +0000

    Basic slider

---
+++ src/background.ts
@@ -0,0 +1,27 @@
+// Listen to messages sent from other parts of the extension.
+const popupMountedHandler = (
+    request : any,
+    sender : chrome.runtime.MessageSender,
+    sendResponse : (response ?: any) => void,
+) =>
+{
+    // onMessage must return "true" if response is async.
+    const isResponseAsync = false;
+
+    if (request.popupMounted)
+    {
+        console.log("background worker notified that Popup.tsx has mounted.");
+    }
+
+    return isResponseAsync;
+};
+
+chrome.runtime.onMessage.addListener(popupMountedHandler);
+
+const tabChangeHandler = (tabId, changeInfo, tab) =>
+{
+    console.log(tabId);
+    console.log(changeInfo);
+    console.log(tab);
+};
+chrome.tabs.onUpdated.addListener(tabChangeHandler);
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,11 +1,27 @@
 import React, { useEffect } from "react";
+import { ToggleSlider } from "react-toggle-slider";
+import { useChromeState } from "../hooks/useChromeState";
 import "./Popup.scss";
 
-export default function Popup() {
-  useEffect(() => {
-    // Example of how to send a message to eventPage.ts.
-    chrome.runtime.sendMessage({ popupMounted: true });
-  }, []);
+export default function Popup()
+{
+    const [ hideNavigation, setHideNavigation ] = useChromeState("hideNavigation", true);
+    const [ hideThumbnails, setHideThumbnails ] = useChromeState("hideThumbnails", true);
+    const [ redirectToHome, setRedirectToHome ] = useChromeState("RedirectToHome", true); // maybe make the redirect page changeable?
 
-  return <div className="popupContainer">Hello, world!</div>;
+    const broadcastPopupMounted = () =>
+    {
+        // Example of how to send a message to background.ts.
+        chrome.runtime.sendMessage({ popupMounted: true });
+    };
+    useEffect(broadcastPopupMounted, []);
+
+    return (
+        <div className="popupContainer">
+            Hello, world!
+            <ToggleSlider active={hideNavigation} onToggle={(state) => setHideNavigation(state)} />
+            <ToggleSlider active={hideThumbnails} onToggle={(state) => setHideThumbnails(state)} />
+            <ToggleSlider active={redirectToHome} onToggle={(state) => setRedirectToHome(state)} />
+        </div>
+    );
 }
