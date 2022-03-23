3b7f9f9e3e6f9b0c1862f98531f54d45cfd4858b
Helena <helena@ips.software>
2022-03-23 14:44:23 +0000

    pop-up styling first draft

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -20,10 +20,25 @@ export default function Popup()
 
 	return (
 		<div className="popupContainer">
-			Hello, world!
-			<ToggleSlider active={hideNavigation} onToggle={(state) => setHideNavigation(state)} />
-			<ToggleSlider active={hideThumbnails} onToggle={(state) => setHideThumbnails(state)} />
-			<ToggleSlider active={redirectFromShorts} onToggle={(state) => setRedirectFromShorts(state)} />
+			<h3>Turn off:</h3>
+			<div className="popupContainer-content">
+				<div className="popup-toggle-section">
+					<h5 className="div-heading">Hide Navigation</h5>
+					<h5 className="div-heading">Hide Thumbnails</h5>
+					<h5 className="div-heading">Redirect from shorts</h5>
+				</div>
+				<div className="popup-toggle-section">
+					<div className="slider">
+						<ToggleSlider active={hideNavigation} onToggle={(state) => setHideNavigation(state)} />
+					</div>
+					<div className="slider">
+						<ToggleSlider active={hideThumbnails} onToggle={(state) => setHideThumbnails(state)} />
+					</div>
+					<div className="slider">
+						<ToggleSlider active={redirectFromShorts} onToggle={(state) => setRedirectFromShorts(state)} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 
