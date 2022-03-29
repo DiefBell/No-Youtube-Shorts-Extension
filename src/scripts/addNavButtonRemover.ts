43d455b68a476317697593ae066a43c2b6060865
diefbell <diefenbaker.bell@gmail.com>
2022-03-29 19:38:31 +0100

    Added logging to user's console

--- src/scripts/addNavButtonRemover.ts
+++ src/scripts/addNavButtonRemover.ts
@@ -5,7 +5,7 @@ export const addNavButtonRemover = () =>
 {
 	if(window.navButtonRemover) return;
 
-	window.navButtonRemover = new MutationObserver((mutations) =>
+	const navButtonRemoverCallback = (mutations : MutationRecord[]) =>
 	{
 		const nodeIsElement = (node : Node) : node is Element => !!(node as Element).matches;
 
@@ -20,12 +20,17 @@ export const addNavButtonRemover = () =>
 			{
 				if(node.matches("ytd-mini-guide-entry-renderer"))
 				{
-					if(node.querySelector("a[title='Shorts']")) node.remove();
+					if(node.querySelector("a[title='Shorts']"))
+					{
+						console.log("No YouTube Shorts: removing navigation button");
+						node.remove();
+					}
 				}
 			}
 		}));
-	});
+	};
 
+	window.navButtonRemover = new MutationObserver(navButtonRemoverCallback);
 	const youtubeRootNode = document.querySelector("ytd-app");
 	window.navButtonRemover.observe(youtubeRootNode, { subtree: true, childList: true });
 };
--- src/scripts/addThumbnailRemover.ts
+++ src/scripts/addThumbnailRemover.ts
@@ -5,7 +5,7 @@ export const addThumbnailRemover = () =>
 {
 	if(window.thumbnailRemover) return;
 
-	window.thumbnailRemover = new MutationObserver((mutations) =>
+	const thumbnailRemoverCallback = (mutations : MutationRecord[]) =>
 	{
 		const nodeIsElement = (node : Node) : node is Element => !!(node as Element).matches;
 
@@ -20,20 +20,26 @@ export const addThumbnailRemover = () =>
 			{
 				if(node.matches("ytd-grid-video-renderer"))
 				{
-					// need to ensure that the item is RENDERED, as well as its DOM node existing
-					setTimeout(() =>
+					const removeThumbnailIfShort = () =>
 					{
 						// YouTube uses custom HTML tags, so selectors won't work
 						// would be nice to find a way of doing this that doesn't require the setTimeout
 						const overlayNodes = node.getElementsByTagName("ytd-thumbnail-overlay-time-status-renderer");
 
-						if(overlayNodes[0]?.matches("[overlay-style='SHORTS']")) node.remove();
-					}, 0);
+						if(overlayNodes[0]?.matches("[overlay-style='SHORTS']"))
+						{
+							console.log("No Youtube Shorts: removing thumbnail");
+							node.remove();
+						}
+					};
+					// need to ensure that the item is RENDERED, as well as its DOM node existing
+					setTimeout(removeThumbnailIfShort, 0);
 				}
 			}
 		}));
-	});
+	};
 
+	window.thumbnailRemover = new MutationObserver(thumbnailRemoverCallback);
 	const youtubeRootNode = document.querySelector("ytd-app");
 	window.thumbnailRemover.observe(youtubeRootNode, { subtree: true, childList: true });
 };
