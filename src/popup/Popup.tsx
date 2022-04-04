d811774836a9c4d8b9d517a1f4812ca134a4b67f
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2022-04-04 21:06:22 +0100

    test commit

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -86,6 +86,7 @@ export default function Popup()
 
 				{ disabledUntil !== null ? (
 					<div>
+						{/* comment */}
 						<button
 							type="button"
 							onClick={() => setDisabledUntil(null)}
