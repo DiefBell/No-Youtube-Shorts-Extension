8605545adee6bd9c12012c8a657ac889381d5879
diefbell <diefenbaker.bell@gmail.com>
2022-11-03 22:51:30 +0000

    Fixed failed bugfix

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -1,17 +1,8 @@
 import { MessagePayload } from "../types/MessagePayload";
 
 export const log = (msg : string, args ?: unknown) =>
-{
-	try
-	{
-		chrome.runtime.sendMessage<MessagePayload, undefined>({
-			type: "LOG",
-			msg,
-			args
-		});
-	}
-	catch(err)
-	{
-		console.error(err);
-	}
-};
+	chrome.runtime.sendMessage<MessagePayload, undefined>({
+		type: "LOG",
+		msg,
+		args
+	});
